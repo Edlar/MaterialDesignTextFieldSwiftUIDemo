c9c5a3b70a638b9b54581d08f2c34070957ff1b2
Eduard Laryushkin <ceo@edlar.ru>
2021-04-16 20:27:07 +0300

    Initial commit

---
+++ Views/MaterialDesignTextFieldSwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaterialDesignTextFieldSwiftUIDemoApp.swift
+//  MaterialDesignTextFieldSwiftUIDemo
+//
+//  Created by Eduard Laryushkin on 16.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct MaterialDesignTextFieldSwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
